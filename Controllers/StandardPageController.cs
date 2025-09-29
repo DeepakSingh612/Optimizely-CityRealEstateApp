72de50e490300ecd31445d1a7229b34c00176aac
Deepak Singh <deepak.singh2314@gmail.com>
2025-09-29 21:46:52 +0530

    Added Standard page

---
+++ Controllers/StandardPageController.cs
@@ -0,0 +1,15 @@
+﻿using CityRealEstate.Models;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityRealEstate.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        [HttpGet]
+        public IActionResult Index(StandardPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
